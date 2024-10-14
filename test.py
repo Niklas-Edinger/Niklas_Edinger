04dc22ad88494ab26d546e8c86e1a3dc393b51e8
niklas.edinger <niklas.edinger@gmail.com>
2024-10-14 11:13:10 +0000

    Aufgabe 1.2 umgesetzt.

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 # This is a python test file!
+
+# 1.2
+
+print('Hallo! Wie heißen Sie?')
+meinName = input()
+print('Wie alt sind Sie in Jahren?')
+meinAlter = input()
+print('Wie viel Geld verdienen Sie jährlich Brutto in Euro?')
+meinGehalt = input()
+
+print('Sie heißen ', meinName, 'sind ', meinAlter ' Jahre alt und verdienen ', meinGehalt, 'Euro im Jahr.')
